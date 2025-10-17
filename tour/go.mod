6206f2b63dd17e486cfdc09b384275bd16c959df
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-10-17 09:17:37 +0100

    feat: add initial Go tour project with basic math examples

---
+++ tour/go.mod
@@ -0,0 +1,3 @@
+module example/tour
+
+go 1.25.0
---
+++ tour/packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(2))
+	fmt.Printf("Now you have %g problems \n", math.Sqrt(9))
+
+}
