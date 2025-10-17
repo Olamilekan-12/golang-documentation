ba9449ef31146bc983c28d452d2c1f1ed1a7cde3
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-10-17 11:32:52 +0100

    feat: add switch statement examples for OS detection and date/time checks

---
+++ tour/switch/switch.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func checkDay() {
+	fmt.Println("When's Saturday? ")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("Too far away")
+	}
+}
+
+func checkHours() {
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+}
+
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("macOS. ")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
+
+	checkDay()
+
+}
