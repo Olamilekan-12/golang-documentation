34a228d941305d3e8e1c69d71c9b58c31ecda22b
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-10-18 13:49:45 +0100

    feat: add slicesFive function demonstrating slice operations with different bounds

--- tour/slices/slices.go
+++ tour/slices/slices.go
@@ -45,9 +45,21 @@ func slicesFour() {
 	fmt.Println(s)
 }
 
+func slicesFive() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	s = s[1:4]
+	fmt.Println(s)
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
+}
+
 func main() {
 	slicesOne()
 	slicesTwo()
 	slicesThree()
 	slicesFour()
+	slicesFive()
 }
